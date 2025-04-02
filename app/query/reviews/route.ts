f71ada9739d4b84e0905ba424d57e6f9c7c084f8
iezll1106 <llizardo@byupathway.edu>
2025-04-02 12:26:07 +0800

    add review.ts

---
+++ app/query/reviews/route.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+import postgres from "postgres";
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
+
+//Get all reviews
+export async function GET() {
+    try {
+        const reviews = await sql`SELECT * FROM reviews;`;
+        return NextResponse.json(reviews);
+    } catch (error) {
+        console.error("Error fetching reviews:", error);
+        return NextResponse.json({ error: "Failed to fetch reviews"}, { status:500 });
+    }
+}
+
+//Create a new review
+export async function POST(req: Request) {
+    try {
+        const { product_id, user_id, rating, comment, created_at } = await req.json();
+
+        const [ reviews ] = await sql`
+            INSERT INTO REVIEWS (product_id, user_id, rating, comment)
+            VALUES (${product_id}, ${user_id}, ${rating}, ${comment})
+            RETURNING *;
+            `;
+        return NextResponse.json(reviews, { status: 201 });
+
+    } catch (error) {
+        console.error("Error adding reviews:", error);
+        return NextResponse.json({ error: "Failed to add review" }, { status: 500} );
+    }
+}
