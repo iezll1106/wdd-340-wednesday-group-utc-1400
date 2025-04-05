6d0cbd8cffccc46357a6148827c1a5df6e1f1654
Liezl <llizardo@byupathway.edu>
2025-04-05 19:59:24 +0800

    edit reviewBoard.tsx

--- app/ui/reviews/reviewBoard.tsx
+++ app/ui/reviews/reviewBoard.tsx
@@ -2,26 +2,32 @@ import { fetchReviews, fetchReviewsByProductId } from "@/app/lib/data";
 import ReviewCard from "./reviewCard";
 
 interface Review {
-    id: string;
-    product_id: string;
-    user_id: string;
-    rating: number;
-    comment: string;
-    created_at: string;
+  id: string;
+  product_id: string;
+  user_id: string;
+  rating: number;
+  comment: string;
+  created_at: string;
 }
 
-export default async function ReviewBoard({product_id} : any) {
-    let reviews;
-    if (product_id) {
-        reviews = await fetchReviewsByProductId(product_id);
-    } else {
-        reviews = await fetchReviews();
-    }
+type Props = {
+  product_id?: string;
+};
 
-    return (
-        <div className={`flex flex-row`}>
-            {reviews.map((review) => (
-                <ReviewCard key={review.id} review={review}/>
-            ))}
-        </div>)
+export default async function ReviewBoard({ product_id }: Props) {
+  let reviews: Review[];
+
+  if (product_id) {
+    reviews = await fetchReviewsByProductId(product_id);
+  } else {
+    reviews = await fetchReviews();
+  }
+
+  return (
+    <div className="flex flex-row">
+      {reviews.map((review: Review) => (
+        <ReviewCard key={review.id} review={review} />
+      ))}
+    </div>
+  );
 }
