78b9136c6a92d22e16ecbfa0b5e9abbb27015421
iezll1106 <llizardo@byupathway.edu>
2025-04-10 16:12:07 +0800

    edit next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
   images: {
-    // Use this ONLY if you're getting issues with image optimization.
-    unoptimized: false, // set to true only if needed for testing
+    // no unoptimized flag
   },
 };
 
