fe22801b75d9afa5be144d0817cb774b147d5d39
TheeJStar <kingoflightj@gmail.com>
2025-04-14 07:59:32 -0700

    fixed the weird spacing in the sellers page and moved the product details on the product card to the bottom

--- app/ui/products/productCard.tsx
+++ app/ui/products/productCard.tsx
@@ -18,22 +18,25 @@ interface ProductProps {
 
 export default function ProductCard({ product }: ProductProps) {
   return (
-      <Link className="border border-gray-200 rounded-lg shadow-lg p-4 bg-white hover:shadow-xl transition max-w-[250px]" href={`/dashboard/products/${product.id}`}>
+      <Link className="flex flex-col justify-between border border-gray-200 rounded-lg shadow-lg p-4 bg-white hover:shadow-xl transition max-w-[250px]" href={`/dashboard/products/${product.id}`}>
         <Image 
           src={product.image_url} 
           alt={product.name} 
           width={300} 
           height={200} 
-          className="rounded-md"
+          className="rounded-md object-cover"
         />
-        <h3 className="text-lg font-bold mt-2">{product.name}</h3>
-        <p>by {fetchSellerName(product.seller_id)}</p>
-        <p className="text-gray-600">{product.description}</p>
-        <div className="flex justify-between items-center mt-3">
-          <span className="text-xl font-semibold text-blue-600">${product.price}</span>
-          <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition">
-            Order Product 
-          </button>
+        <div className="mt-auto">
+          <hr className="mt-2"/>
+          <h3 className="text-lg font-bold mt-2">{product.name}</h3>
+          <p>by {fetchSellerName(product.seller_id)}</p>
+          <p className="text-gray-600">{product.description}</p>
+          <div className="flex justify-between items-center mt-3">
+            <span className="text-xl font-semibold text-blue-600">${product.price}</span>
+            <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition">
+              Order Product 
+            </button>
+          </div>
         </div>
       </Link>
   );
