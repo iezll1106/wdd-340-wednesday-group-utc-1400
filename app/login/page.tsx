d31db1a3d1bbf7e8b13208d9b343e0dcec4253f8
TheeJStar <kingoflightj@gmail.com>
2025-04-17 02:33:34 -0700

    fixed the login page title

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,11 @@
 import { HCHLogo } from '@/app/ui/hch-logo';
 import LoginForm from '@/app/ui/login-form';
 import { Suspense } from 'react';
+import { Metadata } from 'next';
  
+export const metadata: Metadata = {
+  title: 'Login | HCH',
+};
 export default function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
